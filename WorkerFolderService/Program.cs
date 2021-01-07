25e8780a28d70d839f5ace914d2eca1d4cf848ed
askar.khaimuldin <askar.khaimuldin@astanait.edu.kz>
2021-01-07 14:08:50 +0600

    Add project files.

---
+++ WorkerFolderService/Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmailService;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace WorkerFolderService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .UseSerilog()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(hostContext.Configuration).CreateLogger();
+                    var emailConfig = hostContext.Configuration.GetSection("EmailConfiguration").Get<EmailConfiguration>();
+                    services.AddSingleton(emailConfig);
+                    services.AddSingleton<IEmailSender, EmailSender>();
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
